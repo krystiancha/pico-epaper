ed96dd39245bb44c075e301da81a1f4b8135a567
krystianch <krystian@krystianch.com>
2021-03-19 20:00:42 +0100

    Switch to tab indentation

--- src/epaper.c
+++ src/epaper.c
@@ -6,123 +6,133 @@
 #include <pico/critical_section.h>
 #include <pico/epaper.h>
 
-extern const uint8_t lut_faster_vcom[], lut_faster_w2w[], lut_faster_b2w[], lut_faster_w2b[], lut_faster_b2b[];
-extern const uint8_t lut_fastest_vcom[], lut_fastest_w2w[], lut_fastest_b2w[], lut_fastest_w2b[], lut_fastest_b2b[];
+extern const uint8_t lut_faster_vcom[], lut_faster_w2w[], lut_faster_b2w[],
+	lut_faster_w2b[], lut_faster_b2b[];
+extern const uint8_t lut_fastest_vcom[], lut_fastest_w2w[], lut_fastest_b2w[],
+	lut_fastest_w2b[], lut_fastest_b2b[];
 
-void epaper_write_array(struct epaper *display, bool data, const uint8_t *src, size_t len) {
-    if (display->critical_section != NULL) {
-        critical_section_enter_blocking(display->critical_section);
-    }
-    gpio_put(display->dc_pin, data);
-    gpio_put(display->cs_pin, 0);
-    spi_write_blocking(display->spi, src, len);
-    gpio_put(display->cs_pin, 1);
-    if (display->critical_section != NULL) {
-        critical_section_exit(display->critical_section);
-    }
+void epaper_write_array(struct epaper *display, bool data, const uint8_t *src,
+		size_t len) {
+	if (display->critical_section != NULL) {
+		critical_section_enter_blocking(display->critical_section);
+	}
+	gpio_put(display->dc_pin, data);
+	gpio_put(display->cs_pin, 0);
+	spi_write_blocking(display->spi, src, len);
+	gpio_put(display->cs_pin, 1);
+	if (display->critical_section != NULL) {
+		critical_section_exit(display->critical_section);
+	}
 }
 
 void epaper_write(struct epaper *display, bool data, int len, ...) {
-    va_list valist;
-    va_start(valist, len);
-    uint8_t src[len];
-    for (int i = 0; i < len; i++) {
-        src[i] = va_arg(valist, int);
-    }
-    va_end(valist);
+	va_list valist;
+	va_start(valist, len);
+	uint8_t src[len];
+	for (int i = 0; i < len; i++) {
+		src[i] = va_arg(valist, int);
+	}
+	va_end(valist);
 
-    epaper_write_array(display, data, src, len);
+	epaper_write_array(display, data, src, len);
 }
 
 void epaper_command(struct epaper *display, uint8_t command, int len, ...) {
-    va_list valist;
-    va_start(valist, len);
-    uint8_t src[len];
-    for (int i = 0; i < len; i++) {
-        src[i] = va_arg(valist, int);
-    }
-    va_end(valist);
+	va_list valist;
+	va_start(valist, len);
+	uint8_t src[len];
+	for (int i = 0; i < len; i++) {
+		src[i] = va_arg(valist, int);
+	}
+	va_end(valist);
 
-    epaper_write_array(display, 0, &command, 1);
-    epaper_write_array(display, 1, src, len);
+	epaper_write_array(display, 0, &command, 1);
+	epaper_write_array(display, 1, src, len);
 }
 
 void epaper_wait(struct epaper *display) {
-    do {
-        sleep_us(10);  // after 10 uS busy should be low
-    } while (!gpio_get(display->busy_pin));
+	do {
+		sleep_us(10);  // after 10 uS busy should be low
+	} while (!gpio_get(display->busy_pin));
 }
 
 void epaper_update(struct epaper *display, bool partial) {
-    // Reset
-    gpio_put(display->rst_pin, 0);
-    sleep_ms(1);  // 10 uS reset signal should be enough
-    gpio_put(display->rst_pin, 1);
-    epaper_wait(display);
+	// Reset
+	gpio_put(display->rst_pin, 0);
+	sleep_ms(1);  // 10 uS reset signal should be enough
+	gpio_put(display->rst_pin, 1);
+	epaper_wait(display);
 
-    // Booster soft start
-    epaper_command(display, EPAPER_BTST, 3, 0x17, 0x17, 0x17);
+	// Booster soft start
+	epaper_command(display, EPAPER_BTST, 3, 0x17, 0x17, 0x17);
 
-    // Power setting
-    epaper_command(display, EPAPER_PWR, 4, EPAPER_VDS_EN | EPAPER_VDG_EN, EPAPER_VGHL_LV0, 0x2b, 0x2b);
+	// Power setting
+	epaper_command(display, EPAPER_PWR, 4, EPAPER_VDS_EN | EPAPER_VDG_EN,
+		EPAPER_VGHL_LV0, 0x2b, 0x2b);
 
-    // Power on
-    epaper_command(display, EPAPER_PON, 0);
-    epaper_wait(display);
+	// Power on
+	epaper_command(display, EPAPER_PON, 0);
+	epaper_wait(display);
 
-    // Panel setting
-    epaper_command(
-        display, 
-        EPAPER_PSR,
-        1,
-        EPAPER_RES0 | EPAPER_REG_EN | EPAPER_BWR | EPAPER_UD | EPAPER_SHL | EPAPER_SHD_N | EPAPER_RST_N
-    );
+	// Panel setting
+	epaper_command(display, EPAPER_PSR, 1, EPAPER_RES0 | EPAPER_REG_EN
+		| EPAPER_BWR | EPAPER_UD | EPAPER_SHL | EPAPER_SHD_N
+		| EPAPER_RST_N);
 
-    // PLL control
-    epaper_command(display, EPAPER_PLL, 1, 0x3c);
+	// PLL control
+	epaper_command(display, EPAPER_PLL, 1, 0x3c);
 
-    // Resolution setting
-    epaper_command(display, EPAPER_TRES, 4, 0x01, 0x90, 0x01, 0x2c);
+	// Resolution setting
+	epaper_command(display, EPAPER_TRES, 4, 0x01, 0x90, 0x01, 0x2c);
 
-    // VCM_DC setting
-    epaper_command(display, EPAPER_VDCS, 1, 0x28);
+	// VCM_DC setting
+	epaper_command(display, EPAPER_VDCS, 1, 0x28);
 
-    // Vcom and data interval setting
-    epaper_command(display, EPAPER_CDI, 1, display->black_border ? 0x77 : 0x97);
+	// Vcom and data interval setting
+	epaper_command(display, EPAPER_CDI, 1,
+		display->black_border ? 0x77 : 0x97);
 
-    // LUT
-    epaper_write(display, 0, 1, EPAPER_LUTC);
-    epaper_write_array(display, 1, !partial ? lut_faster_vcom : lut_fastest_vcom, 44);
-    epaper_write(display, 0, 1, EPAPER_LUTWW);
-    epaper_write_array(display, 1, !partial ? lut_faster_w2w : lut_fastest_w2w, 42);
-    epaper_write(display, 0, 1, EPAPER_LUTBW);
-    epaper_write_array(display, 1, !partial ? lut_faster_b2w : lut_fastest_b2w, 42);
-    epaper_write(display, 0, 1, EPAPER_LUTWB);
-    epaper_write_array(display, 1, !partial ? lut_faster_w2b : lut_fastest_w2b, 42);
-    epaper_write(display, 0, 1, EPAPER_LUTBB);
-    epaper_write_array(display, 1, !partial ? lut_faster_b2b : lut_fastest_b2b, 42);
+	// LUT
+	epaper_write(display, 0, 1, EPAPER_LUTC);
+	epaper_write_array(display, 1,
+		!partial ? lut_faster_vcom : lut_fastest_vcom, 44);
+	epaper_write(display, 0, 1, EPAPER_LUTWW);
+	epaper_write_array(display, 1,
+		!partial ? lut_faster_w2w : lut_fastest_w2w, 42);
+	epaper_write(display, 0, 1, EPAPER_LUTBW);
+	epaper_write_array(display, 1,
+		!partial ? lut_faster_b2w : lut_fastest_b2w, 42);
+	epaper_write(display, 0, 1, EPAPER_LUTWB);
+	epaper_write_array(display, 1,
+		!partial ? lut_faster_w2b : lut_fastest_w2b, 42);
+	epaper_write(display, 0, 1, EPAPER_LUTBB);
+	epaper_write_array(display, 1,
+		!partial ? lut_faster_b2b : lut_fastest_b2b, 42);
 
-    // Transport old data
-    epaper_write(display, 0, 1, EPAPER_DTM1);
-    epaper_write_array(display, 1, display->previous_buffer, display->height * display->width / 8);
+	// Transport old data
+	epaper_write(display, 0, 1, EPAPER_DTM1);
+	epaper_write_array(display, 1, display->previous_buffer,
+		display->height * display->width / 8);
 
-    // Transport new data
-    epaper_write(display, 0, 1, EPAPER_DTM2);
-    epaper_write_array(display, 1, display->buffer, display->height * display->width / 8);
+	// Transport new data
+	epaper_write(display, 0, 1, EPAPER_DTM2);
+	epaper_write_array(display, 1, display->buffer,
+		display->height * display->width / 8);
 
-    // Swap buffers
-    memcpy(display->previous_buffer, display->buffer, display->height * display->width / 8);
+	// Swap buffers
+	memcpy(display->previous_buffer, display->buffer,
+		display->height * display->width / 8);
 
-    // Display refresh
-    epaper_command(display, EPAPER_DSP, 0);
-    epaper_wait(display);
+	// Display refresh
+	epaper_command(display, EPAPER_DSP, 0);
+	epaper_wait(display);
 
-    // Border floating
-    epaper_command(display, EPAPER_CDI, 1, 0x17);
+	// Border floating
+	epaper_command(display, EPAPER_CDI, 1, 0x17);
 
-    // Power off
-    epaper_command(display, EPAPER_POF, 0);
+	// Power off
+	epaper_command(display, EPAPER_POF, 0);
 
-    // Enter into deep sleep mode
-    epaper_command(display, EPAPER_DSLP, 1, 0xa5);
+	// Enter into deep sleep mode
+	epaper_command(display, EPAPER_DSLP, 1, 0xa5);
 }
--- src/examples/draw_patterns.c
+++ src/examples/draw_patterns.c
@@ -14,73 +14,79 @@ uint8_t display_buffer[BUFFER_SIZE];
 uint8_t prev_display_buffer[BUFFER_SIZE];
 
 struct epaper display = {
-        .spi = spi0,
-        .cs_pin = 6,
-        .rst_pin = 8,
-        .dc_pin = 7,
-        .busy_pin = 9,
-        .width = WIDTH,
-        .height = HEIGHT,
-        .black_border = false,
-        .buffer = display_buffer,
-        .previous_buffer = prev_display_buffer,
+	.spi = spi0,
+	.cs_pin = 6,
+	.rst_pin = 8,
+	.dc_pin = 7,
+	.busy_pin = 9,
+	.width = WIDTH,
+	.height = HEIGHT,
+	.black_border = false,
+	.buffer = display_buffer,
+	.previous_buffer = prev_display_buffer,
 };
 
 int main() {
-    spi_init(spi0, 4000000);
-    gpio_set_function(2, GPIO_FUNC_SPI);
-    gpio_set_function(3, GPIO_FUNC_SPI);
+	spi_init(spi0, 4000000);
+	gpio_set_function(2, GPIO_FUNC_SPI);
+	gpio_set_function(3, GPIO_FUNC_SPI);
 
-    gpio_init(display.cs_pin);
-    gpio_init(display.rst_pin);
-    gpio_init(display.dc_pin);
-    gpio_init(display.busy_pin);
+	gpio_init(display.cs_pin);
+	gpio_init(display.rst_pin);
+	gpio_init(display.dc_pin);
+	gpio_init(display.busy_pin);
 
-    gpio_set_dir(display.cs_pin, GPIO_OUT);
-    gpio_set_dir(display.rst_pin, GPIO_OUT);
-    gpio_set_dir(display.dc_pin, GPIO_OUT);
-    gpio_set_dir(display.busy_pin, GPIO_IN);
+	gpio_set_dir(display.cs_pin, GPIO_OUT);
+	gpio_set_dir(display.rst_pin, GPIO_OUT);
+	gpio_set_dir(display.dc_pin, GPIO_OUT);
+	gpio_set_dir(display.busy_pin, GPIO_IN);
 
-    memset(display_buffer, 0xff, BUFFER_SIZE);
-    memset(prev_display_buffer, 0xff, BUFFER_SIZE);
-    
-    // Display contents of the buffer (clear display to white)
-    epaper_update(&display, false);
-    
-    // Draw diagonal lines
-    for (int x = 0; x < display.width; ++x) {
-        int y = (int) ((float) display.height * (float) x / (float) display.width);
+	memset(display_buffer, 0xff, BUFFER_SIZE);
+	memset(prev_display_buffer, 0xff, BUFFER_SIZE);
 
-        display.buffer[y * display.width / 8 + x / 8] &= ~(0x80 >> (x & 0x07));
-        display.buffer[(display.height - y) * display.width / 8 + x / 8] &= ~(0x80 >> (x & 0x07));
-    }
+	// Display contents of the buffer (clear display to white)
+	epaper_update(&display, false);
 
-    // Display contents of the buffer
-    epaper_update(&display, false);
+	// Draw diagonal lines
+	for (int x = 0; x < display.width; ++x) {
+		int y = (int) ((float) display.height * (float) x
+			/ (float) display.width);
 
-    // Draw cricles
-    for (int x0 = 10; x0 < display.width; x0 += 20) {
-        for (int i = 0; i < 100; ++i) {
-            int x = x0 + 10 * sinf(2.0f * M_PI * i / 100);
-            int y = 10 + 10 * cosf(2.0f * M_PI * i / 100);
-            display.buffer[y * display.width / 8 + x / 8] &= ~(0x80 >> (x & 0x07));
-        }
+		display.buffer[y * display.width / 8 + x / 8]
+			&= ~(0x80 >> (x & 0x07));
+		display.buffer[(display.height - y) * display.width / 8 + x / 8]
+			&= ~(0x80 >> (x & 0x07));
+	}
 
-        // Perform a pratial refresh
-        epaper_update(&display, true);
-    }
+	// Display contents of the buffer
+	epaper_update(&display, false);
 
-    // Draw sine wave
-    for (int x = 0; x < display.width; ++x) {
-        int y = (display.height / 2) + 100 * sin(4.0 * M_PI * x / display.width);
+	// Draw cricles
+	for (int x0 = 10; x0 < display.width; x0 += 20) {
+		for (int i = 0; i < 100; ++i) {
+			int x = x0 + 10 * sinf(2.0f * M_PI * i / 100);
+			int y = 10 + 10 * cosf(2.0f * M_PI * i / 100);
+			display.buffer[y * display.width / 8 + x / 8]
+				&= ~(0x80 >> (x & 0x07));
+		}
 
-        display.buffer[y * display.width / 8 + x / 8] &= ~(0x80 >> (x & 0x07));
-    }
+		// Perform a pratial refresh
+		epaper_update(&display, true);
+	}
 
-    // Display contents of the buffer
-    epaper_update(&display, false);
+	// Draw sine wave
+	for (int x = 0; x < display.width; ++x) {
+		int y = (display.height / 2) + 100 * sin(4.0 * M_PI * x
+			/ display.width);
 
-    while (true) { 
-        tight_loop_contents(); 
-    }
+		display.buffer[y * display.width / 8 + x / 8]
+			&= ~(0x80 >> (x & 0x07));
+	}
+
+	// Display contents of the buffer
+	epaper_update(&display, false);
+
+	while (true) {
+		tight_loop_contents();
+	}
 }
